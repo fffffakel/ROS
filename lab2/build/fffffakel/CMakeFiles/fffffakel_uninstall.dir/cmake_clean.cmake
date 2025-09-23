371ce8729df5d17f2504fe7817ac9879f6e71d59
fffffakel <a.shokolov1@g.nsu.ru>
2025-09-23 14:48:37 +0700

    lab2

--- lab2/build/fffffakel/CMakeFiles/fffffakel_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fffffakel_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fffffakel_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
